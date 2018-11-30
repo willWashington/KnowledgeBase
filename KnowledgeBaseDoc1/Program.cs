090685d53dafdbf11b1a0f1497b819716e51fd1e
w2 <william.washington@live.com>
2018-11-30 10:11:52 -0500

    Add project files.

---
+++ KnowledgeBaseDoc1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KnowledgeBaseDoc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
